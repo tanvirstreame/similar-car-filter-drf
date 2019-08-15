3f56c74737ff32ef1b73b35ffbb3197d11c22e7e
Tanvir Islam Streame <streametanvir@gmail.com>
2019-08-16 01:10:17 +0600

    remove print from view and unnecessary spaces

--- vehicle/views.py
+++ vehicle/views.py
@@ -11,6 +11,4 @@ class VehicleList(generics.ListCreateAPIView):
 
 class SimilerVehilceList(generics.ListCreateAPIView):
   serializer_class = VehicleSerializer
-  # queryset = Vehicle.objects.all()
-  queryset = Vehicle.objects.filter( price__lte=20000, odometer_reading__lte=200).order_by("price","odometer_reading")
-  print(queryset)
+  queryset = Vehicle.objects.filter(price__lte=20000, odometer_reading__lte=200).order_by("price", "odometer_reading")
